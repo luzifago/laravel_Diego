109464081595fe64a786d3d7551c0b3a7f4fc910
luzifago <andradelogystic@gmail.com>
2023-11-16 18:14:54 -0500

    hola

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <h1 class="bg-black text-white text-center">Hola Mundo Desde Laravel!</h1>
+        
+    </body>
+</html>
